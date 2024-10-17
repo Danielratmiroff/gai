000d57de12765546ca8c0a7f922d6c861d1f30f4
danielr <daniel.ratmiroff@sdui.de>
2024-10-17 11:48:03 +0200

    Implement display choices and commit message writing

--- main.py
+++ main.py
@@ -5,6 +5,7 @@ import yaml
 from groq import Groq
 
 from api.groq_api import GroqClient
+from src.display_choices import DisplayChoices
 from src.commit import Commit
 from src.prompts import Prompts
 
@@ -18,11 +19,17 @@ class Main:
     def __init__(self):
         self.Commit = Commit()
         self.Prompt = Prompts()
+        self.DisplayChoices = DisplayChoices()
 
-        self.loadConfig()
+        self.load_config()
+        self.init_groq_client()
         self.run()
 
-    def loadConfig(self):
+    def init_groq_client(self):
+        self.groq_chat_client = GroqClient(
+            self.get_api_key(), self.model, self.temperature, self.max_tokens)
+
+    def load_config(self):
         with open("config.yaml", "r") as file:
             config = yaml.safe_load(file)
 
@@ -30,7 +37,7 @@ class Main:
         self.temperature = config['temperature']
         self.max_tokens = config['max_tokens']
 
-    def getApiKey(self):
+    def get_api_key(self):
         api_key = os.environ.get("GROQ_API_KEY")
 
         if api_key is None:
@@ -40,17 +47,17 @@ class Main:
         return self.api_key
 
     def run(self):
-        groq_chat_client = GroqClient(
-            self.getApiKey(), self.model, self.temperature, self.max_tokens)
-
         git_diffs = self.Commit.get_diffs()
         build_prompt = self.Prompt.build_commit_message_prompt(git_diffs)
 
-        print(build_prompt)
+        # print(build_prompt)
         print(f"token count: {len(build_prompt.split())}")
 
-        response = groq_chat_client.get_chat_completion(build_prompt)
-        print(response)
+        response = self.groq_chat_client.get_chat_completion(build_prompt)
+
+        commit_message = self.DisplayChoices.run(response)
+
+        self.Commit.commit_changes(commit_message)
 
 
 if __name__ == "__main__":
--- src/commit.py
+++ src/commit.py
@@ -5,24 +5,22 @@ class Commit:
     model = None
 
     def __init__(self):
-        self.cmd = "git --no-pager diff --cached --ignore-space-change"
+        self.diff_cmd = "git --no-pager diff --cached --ignore-space-change"
+        self.show_committed_cmd = "git diff --cached --name-only"
         pass
 
-    def read_diff_file(self, diff_file_path):
+    def read_diff_file(self, diff_file_path: str) -> str:
         with open(diff_file_path, "r") as file:
             return file.read()
 
-    def clean_file(self, diff_file_path):
+    def clean_file(self, diff_file_path: str):
         if os.path.exists(diff_file_path):
             os.remove(diff_file_path)
 
     def get_diffs(self) -> str:
-        # Stage all changes to be able to get the diff
-        os.system("git add .")
-
         # We store the diff in a file to avoid printing it to the console
         diff_file_path = os.path.join(os.getcwd(), "diff.txt")
-        os.system(f"{self.cmd} --output={diff_file_path}")
+        os.system(f"{self.diff_cmd} --output={diff_file_path}")
 
         self_diff = self.read_diff_file(diff_file_path)
 
@@ -30,3 +28,12 @@ class Commit:
         self.clean_file(diff_file_path)
 
         return self_diff
+
+    def commit_changes(self, commit_message: str):
+        print(f"Committing changes with message: {commit_message}")
+
+        os.system(f"git commit -m '{commit_message}'")
+
+        # Print committed changes
+        os.system(self.show_committed_cmd)
+        print("Changes committed successfully")
