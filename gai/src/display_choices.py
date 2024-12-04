d3b9574dea62336a6a1d86097dd11ba9fbb9bc53
bot <danielratmiroff@gmail.com>
2024-12-04 07:45:01 +0100

    Remove debug prints from display choices

--- gai/src/display_choices.py
+++ gai/src/display_choices.py
@@ -53,9 +53,6 @@ class DisplayChoices:
             create_user_message(user_msg)
         ]
 
-        print("\n" + sys_prompt)
-        print(user_msg)
-
         response = ai_client(
             user_message=messages.copy(),
         )
