b4071d7ccbba8fcac0ed5b545750ec257f15d79d
bot <danielratmiroff@gmail.com>
2025-02-21 12:43:38 +0100

    Modify tests/main.py to pass command line arguments to pytest

--- tests/main.py
+++ tests/main.py
@@ -1,9 +1,11 @@
+import sys
 import pytest
 
 
 def main():
     print("Running tests...")
-    pytest.main()
+    # Pass all command line arguments to pytest
+    sys.exit(pytest.main(sys.argv[1:]))
 
 
 if __name__ == "__main__":
